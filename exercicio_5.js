a988c43c1e4fa4ef4ce1b1e1e8166cdf8568dfab
Zubizeru <jose.ehmendes@senacsp.edu.br>
2024-09-04 18:54:43 -0300

    atualizando exercicio 5

--- exercicio_5.js
+++ exercicio_5.js
@@ -1,36 +1,58 @@
+     /* Crie um algoritmo que leia 20 números inteiros e armazene-os num 
+vetor. Armazene os números pares no vetor PAR e os números IMPARES 
+no vetor impar. Deve-se no final mostrar os três vetores. */
 function executarExercicio5() {
-    /*Crie um algoritmo que leia 20 números inteiros e armazene-os num 
-    vetor. Armazene os números pares no vetor PAR e os números IMPARES 
-    no vetor impar. Deve-se no final mostrar os três vetores */
-
     let impar = [];
     let numero = [];
     let par = [];
-    let mensagem = `Considerando os 12% de INSS descontados: \n`;
+    let mensagem = "Números informados:\n";
 
-    // Leitura das notas;
+    // Leitura dos números
     for (let i = 0; i < 20; i++) {
-        numero.push(parseInt(prompt(`Informe o ${i + 1}ª salário:`)));
+        numero.push(parseInt(prompt(`Informe o ${i + 1}º número:`)));
     }
 
-    // Verificação de par ou impar;
-    for (let i = 0; i < numero.length; i++) { // Verifica os números que o usuário colocou; 
-        if (numero[i] / 2 == 0) {             // Se número for 
-            impar.push(numero[i]);            // Puxa para o vetor impar;
-        } else {                              // Se não...
-        par.push(numero[i]);                 // Puxa para o vetor par;
+    // Verificação de par ou ímpar e construção da mensagem de números informados
+    for (let i = 0; i < numero.length; i++) {
+        mensagem += numero[i];
+
+        if (i === numero.length - 1) {
+            mensagem += ".";
+        } else {
+            mensagem += ", ";
+        }
+
+        if (numero[i] % 2 === 0) {   // Verifica se o número é par
+            par.push(numero[i]);
+        } else {                     // Caso contrário, é ímpar
+            impar.push(numero[i]);
         }
     }
 
-    // Construção da mensagem 
-    for (let i = 0; i < 5; i++) {                      // Verifica os sálarios que o usuário colocou.
-        mensagem += `O ${i + 1}° sálario é:`;          //Exibe os salarios em uma string para serem apresentadas ao usuário na frente da string da mensagem.
-        if (i === (cal.length - 1)) {                  //Se for a ultima iteração...
-            mensagem += ` R$${cal[i].toFixed(2)}.`;    //Vai colocar ponto.
-        } else {                                       //Se não for a ultima iteração...
-            mensagem += ` R$${cal[i].toFixed(2)}; \n`; //Vai colocar virgula e espaço.
+    // Construção da mensagem de números pares
+    mensagem += "\nNúmeros pares:\n";
+    for (let i = 0; i < par.length; i++) {
+        mensagem += par[i];
+
+        if (i === par.length - 1) {
+            mensagem += ".";
+        } else {
+            mensagem += ", ";
         }
     }
 
+    // Construção da mensagem de números ímpares
+    mensagem += "\nNúmeros ímpares:\n";
+    for (let i = 0; i < impar.length; i++) {
+        mensagem += impar[i];
+
+        if (i === impar.length - 1) {
+            mensagem += ".";
+        } else {
+            mensagem += ", ";
+        }
+    }
+
+    // Exibição da mensagem completa
     alert(mensagem);
 }
