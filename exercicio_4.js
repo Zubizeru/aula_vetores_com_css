80d7208dc8af8f81c42ddfeb997f35612668f0ac
Zubizeru <jose.ehmendes@senacsp.edu.br>
2024-09-09 20:18:09 -0300

    feat: atualizando exercicio 4

--- exercicio_4.js
+++ exercicio_4.js
@@ -2,22 +2,21 @@ function executarExercicio4() {
     /* Crie um algoritmo que leia o salário bruto de 5 pessoas, e ao final, 
  mostre o salário líquido de todos, considerando 12% de INSS a descontar */
     let salario = [];
-    let cal = [];
     let mensagem = `Considerando os 12% de INSS descontados: \n`;
 
     // Leitura das notas
     for (let i = 0; i < 5; i++) {
         salario.push(prompt(`Informe o ${i + 1}ª salário:`));
-        cal[i] = parseFloat(salario[i] - (salario[i] * 12 / 100))
+        salario[i] = parseFloat(salario[i] - (salario[i] * 12 / 100))
     }
 
     // Construção da mensagem 
     for (let i = 0; i < 5; i++) {                      // Verifica os sálarios que o usuário colocou.
         mensagem += `O ${i + 1}° sálario é:`;          //Exibe os salarios em uma string para serem apresentadas ao usuário na frente da string da mensagem.
-        if (i === (cal.length - 1)) {                  //Se for a ultima iteração...
-            mensagem += ` R$${cal[i].toFixed(2)}.`;    //Vai colocar ponto.
+        if (i === (salario.length - 1)) {                  //Se for a ultima iteração...
+            mensagem += ` R$${salario[i].toFixed(2)}.`;    //Vai colocar ponto.
         } else {                                       //Se não for a ultima iteração...
-            mensagem += ` R$${cal[i].toFixed(2)}; \n`; //Vai colocar virgula e espaço.
+            mensagem += ` R$${salario[i].toFixed(2)}; \n`; //Vai colocar virgula e espaço.
         }
     }
 
