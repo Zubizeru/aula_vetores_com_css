e1b98f01ca3b9242fcec2c9bed8a65547c28908a
José Elias Herminio Mendes <jose.ehmendes@senacsp.edu.br>
2024-09-11 18:38:38 -0300

    Atualizando o exercicio_4_v1.js

--- exercicio_4_v1.js
+++ exercicio_4_v1.js
@@ -1,29 +1,32 @@
 /* Crie um algoritmo que leia o salário bruto de 5 pessoas, e ao final, 
 mostre o salário líquido de todos, considerando 12% de INSS a descontar */
 function executarExercicio4_v1() {
-    let salario = lerSalario(5);
-    let mensagem = construirMensagem4(salario);
-    alert(mensagem);
+    let salarios = lerSalario(5);
+    exibirSalarios(salarios);
 }
 
 function lerSalario(quantidade) {
-    let salario = [];
+    let salarios = [];
     for (let i = 0; i < quantidade; i++) {
-        salario.push(parseFloat(prompt(`Informe o ${i + 1}° salário:`)));
-        salario[i] = salario[i] - (salario[i] * 12 / 100); // Calcula o salário líquido
+        let salario
+        do {
+        salario = (parseFloat(prompt(`Informe o ${i + 1}° salário:`)));
+        } while (isNaN(salario))
+        salarios.push(salario)
+        salarios[i] = salarios[i] - (salarios[i] * 12 / 100); // Calcula o salário líquido
     }
-    return salario;
+    return salarios;
 }
 
-function construirMensagem4(salario) {
+function exibirSalarios(salarios) {
     let mensagem = "";
-    for (let i = 0; i < salario.length; i++) { // Use cal.length para acessar o tamanho do array
+    for (let i = 0; i < salarios.length; i++) { // Use cal.length para acessar o tamanho do array
         mensagem += `O ${i + 1}° salário é: `;
-        if (i === (salario.length - 1)) {
-            mensagem += `R$${salario[i].toFixed(2)}.`; // Último elemento, adicione um ponto.
+        if (i === (salarios.length - 1)) {
+            mensagem += `R$${salarios[i].toFixed(2)}.`; // Último elemento, adicione um ponto.
         } else {
-            mensagem += `R$${salario[i].toFixed(2)}; \n`; // Outros elementos, adicione vírgula e nova linha.
+            mensagem += `R$${salarios[i].toFixed(2)}; \n`; // Outros elementos, adicione vírgula e nova linha.
         }
     }
-    return mensagem;
+    alert(mensagem);
 }
